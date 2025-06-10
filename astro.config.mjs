7c51e025e3d1bdc063734a8a87250fbf5b023c7a
.voidrc <voidrc_10101@proton.me>
2025-06-10 23:29:17 +0500

    URL Issue::Test2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://voidborne-archive.netlify.app',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
