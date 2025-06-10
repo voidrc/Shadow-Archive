fe4159dc28176bff9b964509ebcf95bdeb834902
.voidrc <voidrc_10101@proton.me>
2025-06-11 01:00:51 +0500

    common

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,43 +1,11 @@
 import { defineCollection, z } from 'astro:content';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-const blog = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.date(),
-		updatedDate: z.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
-
-const projects = defineCollection({
-=======
 const notes = defineCollection({
 	type: 'content',
->>>>>>> parent of c4d7c0d (revert)
-=======
-const notes = defineCollection({
-	type: 'content',
->>>>>>> parent of c4d7c0d (revert)
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		date: z.date(),
-<<<<<<< HEAD
-<<<<<<< HEAD
-		technologies: z.array(z.string()),
-		sourceUrl: z.string().url(),
-		demoUrl: z.string().url().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
-
-export const collections = { blog, projects }; 
-=======
-=======
->>>>>>> parent of c4d7c0d (revert)
 		image: z.string().optional()
 	})
 });
@@ -58,9 +26,4 @@ const projects = defineCollection({
 export const collections = {
 	notes,
 	projects
-<<<<<<< HEAD
-}; 
->>>>>>> parent of c4d7c0d (revert)
-=======
-}; 
->>>>>>> parent of c4d7c0d (revert)
+};
