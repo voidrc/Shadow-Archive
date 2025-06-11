19f5e889d0d8d647404733943e937723f179e750
.voidrc <voidrc_10101@proton.me>
2025-06-11 19:31:20 +0500

    test2

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.string().transform((str) => new Date(str)),
+		heroImage: z.string().optional(),
+	}),
+});
+
+const projects = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.string().transform((str) => new Date(str)),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { blog, projects }; 
