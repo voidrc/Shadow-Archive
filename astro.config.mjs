565a772f1ba6504db9a9800d4d11695d60200d8f
.voidrc <voidrc_10101@proton.me>
2025-06-14 04:16:27 +0500

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://voidborne-archive.netlify.app',
+  site: 'https://shadow-archive.netlify.app',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Voidborne Archive';
+export const SITE_TITLE = 'Shadow Archive';
 export const SITE_DESCRIPTION = 'Collection of Cyber-Security Notes and Projects by \{.voidrc\}';
